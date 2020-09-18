56d3e1da70b1c0ab9270d68a4e6cdb446f6c6397
Francesco Bottino <francesco.bottino96@gmail.com>
2020-09-18 12:00:53 +0200

    added state setter

--- chronometers-extensions/src/main/java/it/uniparthenope/francescobottino001/chronometers_extensions/PausableChronometer.kt
+++ chronometers-extensions/src/main/java/it/uniparthenope/francescobottino001/chronometers_extensions/PausableChronometer.kt
@@ -74,4 +74,37 @@ open class PausableChronometer @JvmOverloads constructor(
         fun onStateEmpty()
     }
     var stateListener: StateListener? = null
+
+    fun setChronometerState(state: State, seconds: Long = 0L) {
+        when(state) {
+            State.EMPTY, State.IDLE, State.PAUSED -> {
+                super.stop()
+
+                if( state == State.EMPTY ) {
+                    totalElapsedSeconds = 0L
+                    setText(0L)
+                } else {
+                    totalElapsedSeconds = seconds
+                    setText(seconds)
+                }
+
+                currentState = state
+                when (state) {
+                    State.IDLE -> stateListener?.onStateIdle()
+                    State.PAUSED -> stateListener?.onStatePaused()
+                    State.EMPTY -> stateListener?.onStateEmpty()
+                    else -> {}
+                }
+            }
+            State.RUNNING -> {
+                totalElapsedSeconds = seconds
+                base = SystemClock.elapsedRealtime() + totalElapsedSeconds
+
+                super.start()
+
+                currentState = State.RUNNING
+                stateListener?.onStateRunning()
+            }
+        }
+    }
 }
